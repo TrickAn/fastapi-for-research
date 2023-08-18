a8350bec23c55673fbb02bce3df3ef7118f2cc69
redeck1 <redeck111@mail.ru>
2023-08-18 11:09:45 +0500

    add form validation

--- Backend/routes/users.py
+++ Backend/routes/users.py
@@ -37,8 +37,9 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 def read_user_by_name(user_name: str, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_name(db, user_name=user_name)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+        raise HTTPException(status_code=200, detail="User not found")
+    # return db_user
+    raise HTTPException(status_code=200, detail="User found")
 
 
 @Users.put("/{user_id}/", response_model=Backend.schemas.users.User)
--- Frontend/src/components/Form.jsx
+++ Frontend/src/components/Form.jsx
@@ -1,46 +1,97 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { DatabaseContext } from "../context/database/databaseContext";
+import axios from "axios";
+
+const url = "http://127.0.0.1:8000";
 
 const Form = () => {
   const [nameValue, setNameValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
+  const [userDirty, setUserDirty] = useState(false);
+  const [userError, setUserError] = useState("Это поле не может быть пустым");
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [passwordError, setPasswordError] = useState(
+    "Это поле не может быть пустым"
+  );
+  const [formValid, setFormValid] = useState(false)
   const db = useContext(DatabaseContext);
 
   const submitHandler = async (event) => {
     event.preventDefault();
     db.addUser(nameValue, passwordValue)
-    .then(() => {
-      setNameValue('')
-      setPasswordValue('')
-    })
-    .catch((error) => {
-      console.log(error.response.data)
-    })
-    
+      .then(() => {
+        setNameValue("");
+        setPasswordValue("");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
   };
+
+  const blurHandler = async (e) => {
+    if (e.target.name === "user") {
+      setUserDirty(true);
+      if (e.target.value) {
+        const response = await axios.get(
+          `${url}/users/name/${e.target.value}/`
+        );
+        if (response.data.detail === "User found") {
+          setUserError("Это имя уже занято");
+        } else {
+          setUserError("");
+        } 
+      } else {
+          setUserError("Это поле не может быть пустым")
+        }
+    } else {
+      setPasswordDirty(true);
+      if (e.target.value) {
+        setPasswordError("");
+      } else {
+        setPasswordError("Это поле не может быть пустым")
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (userError || passwordError) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+  }, [userError, passwordError])
 
   return (
     <div className="container mt-3 mb-3">
       <form onSubmit={submitHandler}>
         <div className="form-group">
           <input
+            name="user"
+            onBlur={(e) => blurHandler(e)}
             type="text"
             className="form-control mt-3"
             placeholder="Имя пользователя"
             value={nameValue}
             onChange={(e) => setNameValue(e.target.value)}
           />
+          {userDirty && userError && (
+            <div className="text-danger">{userError}</div>
+          )}
         </div>
         <div className="form-group">
           <input
+            name="password"
+            onBlur={(e) => blurHandler(e)}
             type="text"
             className="form-control mt-3"
             placeholder="Пароль"
             value={passwordValue}
             onChange={(e) => setPasswordValue(e.target.value)}
           />
+          {passwordDirty && passwordError && (
+            <div className="text-danger">{passwordError}</div>)}
         </div>
-        <button type="submit" className="btn btn-primary mt-3">
+        <button disabled={!formValid} type="submit" className="btn btn-primary mt-3">
           Подтвердить
         </button>
       </form>
