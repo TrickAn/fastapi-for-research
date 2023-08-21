662b703bab609ec2c5e05dfa3b7c410a35b262e7
redeck1 <redeck111@mail.ru>
2023-08-21 09:45:09 +0500

    Phrase additor validation

--- Frontend/src/components/PhraseAdditor.jsx
+++ Frontend/src/components/PhraseAdditor.jsx
@@ -1,54 +1,82 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AdditorContext } from "../context/additor/additorContext";
 import { DatabaseContext } from "../context/database/databaseContext";
 
 export const PhraseAdditor = () => {
   const [phrase, setPhrase] = useState("");
+  const [pharseError, setError] = useState("Это поле не может быть пустым");
+  const [errorShow, setErrorShow] = useState(false);
   const { addPhrase } = useContext(DatabaseContext);
   const { state, hide } = useContext(AdditorContext);
 
+  useEffect(() => {
+    setErrorShow(false)
+    setPhrase("");
+    setError("Это поле не может быть пустым");
+  }, [state])
+
   const submitHandler = async (event) => {
     event.preventDefault();
-    addPhrase(state.index, state.id, phrase)
-    setPhrase("")
-    hide()
-
-  
-
+    if (!phrase) {
+      setErrorShow(true)
+    } else { 
+      addPhrase(state.index, state.id, phrase);
+      hideElem()
+    }
   };
 
-const hideElem = () => {
-    setPhrase("")
-    hide()
-  }
+  const hideElem = () => {
+    hide();
+  };
 
   if (!state.show) {
-    return null
+    return null;
   }
 
   return (
-    <div
-      className="position-fixed w-100 h-100 z-2 d-flex align-items-center justify-content-center top-0 left-0"
-      style={{
-        background: "rgba(0, 0, 0, 0.5)",
-      }}
-    >
-      <div className="bg-white rounded-3 p-3 pt-5 position-relative" style={{minWidth: "500px"}}>
-        <button onClick={() => {hideElem()}} type="button" className="btn btn-outline-secondary text-dark position-absolute" style={{right: "3px", top: "3px"}}>
-          &times;
-        </button>
+    <>
+      <button
+        onClick={() => {
+          hideElem();
+        }}
+        className="btn-tinted"
+      ></button>
+      <div
+        className="position-fixed d-flex top-0 left-0 z-2"
+        style={{
+          marginTop: "300px",
+          marginLeft: "calc(50% - 250px)",
+        }}
+      >
+        <div
+          className="bg-white rounded-3 p-3 pt-5 position-relative"
+          style={{ minWidth: "500px" }}
+        >
+          <button
+            onClick={() => {
+              hideElem();
+            }}
+            type="button"
+            className="btn-close-additor position-absolute"
+            style={{ right: "3px", top: "3px" }}
+          >
+            &times;
+          </button>
           <form className="form-group" onSubmit={submitHandler}>
             <input
               type="text"
-              className="form-control mb-2"
+              className="form-control"
               placeholder="Введите фразу"
               autoFocus="autofocus"
               onChange={(e) => setPhrase(e.target.value)}
             />
-            <button className="btn btn-primary">Добавить</button>
+            {pharseError && errorShow && (
+              <div className="text-danger position-absolute">{pharseError}</div>
+            )}
+            <button className="btn btn-primary mt-4">Добавить</button>
           </form>
-
+        </div>
       </div>
-    </div>
+    </>
   );
 };
